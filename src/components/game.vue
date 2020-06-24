9b3824baf59a2b7472e29848de8688559221ab49
丁亚辉 <2294211995@qq.com>
2020-06-24 15:17:51 +0800

    first commit

---
+++ src/components/game.vue
@@ -0,0 +1,27 @@
+/**
+* @author 水印红枫
+* @date 2020/6/24 15:12
+* @Description:
+*/
+
+<template>
+  <div id="game">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'game',
+  data() {
+    return {}
+  },
+  mounted() {
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+  #game {
+  }
+</style>
